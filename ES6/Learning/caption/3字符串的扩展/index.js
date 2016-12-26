a733135db8228b8103a4e4ab47613e5992ce4a5f
Vincent <1312065793@qq.com>
2016-12-26 18:41:52 +0800

    xxxxxxx

--- ES6/Learning/caption/3字符串的扩展/index.js
+++ ES6/Learning/caption/3字符串的扩展/index.js
@@ -33,3 +33,13 @@ var s="hello world";
 s.startsWith('hello')//true
 s.endsWith('!') //true
 s.includes('o')//true
+
+//这三个方法都支持第二个参数，表示开始搜索的位置。 
+var s = 'Hello world!';
+
+s.startsWith('world', 6) // true
+s.endsWith('Hello', 5) // true
+s.includes('Hello', 6) // false
+
+// 上面代码表示，使用第二个参数n时，endsWith的行为与其他两个方法有所不同。它针对前n个字符，而其他两个方法针对从第n个位置直到字符串结束。
+
